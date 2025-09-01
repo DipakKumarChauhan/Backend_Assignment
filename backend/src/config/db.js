3e05b0cea06de7956b78d78014e550c46ad15497
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2025-09-01 16:38:58 +0530

    Url parser removed

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewURLParser: true,
-            useUnifiedTopology: true
+            
         });
         console.log("Mongo Db connected Successfully");;
     } catch(error){
